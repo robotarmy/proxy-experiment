a3b049a9606f86093b2ed5a217b7e8d41f51812f
Curtis Schofield <o_o@TomSwift.local>
2012-09-07 15:20:54 -0700

    experimental

--- modules/site.rb
+++ modules/site.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 require 'sinatra/base'
 require 'riak'
-
-
-
+require 'yajl'
+require 'uri'
+require "base64"
 
 $client = Riak::Client.new(:http_backend => :Excon)
 
@@ -26,19 +26,26 @@ class Site < Sinatra::Base
     p env = request.env
 
     p device_bucket = params['deviceSerial']
-    p device_request = params['qrcodes']
+    p okey =params['qrcodes'] 
+    p device_request_key = Base64.encode64(okey).chomp # drop newline from base64 encodeing scheme
     bucket = $client.bucket(device_bucket)
-    object = bucket.get_or_new(device_request)
+    object = bucket.get_or_new(device_request_key)
     attributes = params.dup # take whatever sinatra parses
     attributes.delete('file') # favor raw post data
     attributes.delete('splat') # * glob
     attributes.delete('captures') # * glob 
+    attributes['original-request-remote-addr']      = env['REMOTE_ADDR']
+    attributes['original-request-scheme']         = env['rack.url_scheme']
+    attributes['unencoded-key']    = okey
     attributes['raw-request-uri']  = env['REQUEST_URI'] # usefull for replay
-    attributes['post-body'] = raw_post  # useful for replay
+    attributes['post-body'] = Base64.encode64(raw_post)  # useful for replay
     attributes['requested?'] = 0   # has this REQUEST been forwarded?
-    p attributes
-    object.data = attributes
+    #p attributes
+    object.key = device_request_key
+    object.raw_data = str = Yajl::Encoder.encode(attributes)
+    #p str
     object.content_type = 'application/json'
+    p object
     object.store
  
     ret = "Nasrudin was riding on his donkey..."
